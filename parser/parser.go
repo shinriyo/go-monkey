5277ffbb14a36ce73c6b8378553551a7b1b08417
Masaya Myojin <m.myojin0425@gmail.com>
2017-01-18 21:38:35 +0900

    My First parser

---
+++ parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"github.com/mmyoji/go-monkey/ast"
+	"github.com/mmyoji/go-monkey/lexer"
+	"github.com/mmyoji/go-monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
