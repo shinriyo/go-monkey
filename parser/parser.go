507d0a0e9266b0a563b36160afda95e198fc829e
Masaya Myojin <m.myojin0425@gmail.com>
2017-01-19 21:37:44 +0900

    Add two types of func type

--- parser/parser.go
+++ parser/parser.go
@@ -8,6 +8,11 @@ import (
 	"github.com/mmyoji/go-monkey/token"
 )
 
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
+)
+
 type Parser struct {
 	l *lexer.Lexer
 
