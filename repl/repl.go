c97a639054b17855ec78936e3cfeb04f8178af4f
Masaya Myojin <m.myojin0425@gmail.com>
2017-02-07 11:00:50 +0900

    REPL with Integer! :tada:

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/mmyoji/go-monkey/evaluator"
 	"github.com/mmyoji/go-monkey/lexer"
 	"github.com/mmyoji/go-monkey/parser"
 )
@@ -35,8 +36,12 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
