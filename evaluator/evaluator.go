0b3e3d3c17cb85172d370fdfeb4f657a4814d65d
Masaya Myojin <m.myojin0425@gmail.com>
2017-02-07 10:58:26 +0900

    Eval Integer

---
+++ evaluator/evaluator.go
@@ -0,0 +1,34 @@
+package evaluator
+
+import (
+	"github.com/mmyoji/go-monkey/ast"
+	"github.com/mmyoji/go-monkey/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+// Private functions from here
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
