6c10e05c86f656e2a6e1059f022c5df6e1d4c1e6
Masaya Myojin <m.myojin0425@gmail.com>
2017-02-08 22:50:28 +0900

    Closures also works!

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -260,6 +260,18 @@ func TestFunctionApplication(t *testing.T) {
 	}
 }
 
+func TestClosures(t *testing.T) {
+	input := `
+let newAdder = fn(x) {
+	fn(y) { x + y };
+};
+
+let addTwo = newAdder(2);
+addTwo(2);`
+
+	testIntegerObject(t, testEval(input), 4)
+}
+
 // Private functions from here
 
 func testEval(input string) object.Object {
